f749a3db54424f5216ef62a7e68fc72f70c3ba11
Stel Abrego <stel@stel.codes>
2021-12-01 22:15:26 -0800

    Make lid check less hardware dependent

--- scripts/check-clamshell-sway.sh
+++ scripts/check-clamshell-sway.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-if grep -q open /proc/acpi/button/lid/LID0/state; then
+
+if [[ -e /proc/acpi/button/lid/LID/state ]]; then
+  lid_state=/proc/acpi/button/lid/LID/state
+elif [[ -e /proc/acpi/button/lid/LID0/state ]]; then
+  lid_state=/proc/acpi/button/lid/LID0/state
+else
+  echo "Lid state file could not be found"
+  exit 1
+fi
+
+if grep -q open $lid_state; then
   swaymsg output "eDP-1" enable
 else
   swaymsg output "eDP-1" disable
