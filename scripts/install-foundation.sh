f2c3be0bf9b5f3cc6067c9a1967bda9a84aa112c
Stel Abrego <stel@stel.codes>
2021-12-02 11:09:25 -0800

    Add to base packages

--- scripts/install-foundation.sh
+++ scripts/install-foundation.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-bullseye_core=(tmux udisks2 curl vim trash-cli git)
-# Missing: starship babashka dust
+bullseye_core=(tmux udisks2 curl vim trash-cli git restic)
+# Missing: starship dust
 # sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --bin-dir ~/.local/bin
 sudo apt update
 sudo apt install ${bullseye_core[*]}
 
-bullseye_sway=(sway kitty wofi wl-clipboard wtype fonts-noto-color-emoji)
+bullseye_sway=(sway kitty wofi wl-clipboard wtype fonts-noto-color-emoji libinput-tools)
 # Missing: wl-sunset rofimoji
 # pip install --user rofimoji
 read -p "Do you want to install sway packages from bullseye? [Y/n] " input
@@ -17,7 +17,7 @@ else
   echo "Skipping sway packages..."
 fi
 
-bullseye_coding=(clojure nodejs python3-pip)
+bullseye_coding=(clojure nodejs npm python3-pip)
 # Missing: babashka
 read -p "Do you want to install coding packages from bullseye? [Y/n] " input
 
