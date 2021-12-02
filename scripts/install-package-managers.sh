d722b2b95df98bf7b7794634c1e271297b964a69
Stel Abrego <stel@stel.codes>
2021-12-01 22:14:17 -0800

    Remove nix installation

--- scripts/install-package-managers.sh
+++ scripts/install-package-managers.sh
@@ -9,4 +9,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Nix (multi-user mode)
-sh <(curl -L https://nixos.org/nix/install) --daemon
+# sh <(curl -L https://nixos.org/nix/install) --daemon
