8cadeaffc4c5d0008d5a8a7895d4a3f71267d1fa
Stel Abrego <stel@stel.codes>
2021-12-07 15:53:02 -0800

    Add base packages

--- scripts/install-foundation.sh
+++ scripts/install-foundation.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-bullseye_core=(tmux udisks2 curl vim trash-cli git restic)
+bullseye_core=(tmux udisks2 curl vim trash-cli git restic htop bat fd-find ripgrep tldr)
 # Missing: starship dust
 # sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --bin-dir ~/.local/bin
 sudo apt update
 sudo apt install ${bullseye_core[*]}
 
-bullseye_sway=(sway kitty wofi wl-clipboard wtype fonts-noto-color-emoji libinput-tools)
+bullseye_sway=(sway kitty wofi wl-clipboard wtype xwayland fonts-noto-color-emoji libinput-tools brightnessctl chromium)
 # Missing: wl-sunset rofimoji
 # pip install --user rofimoji
+# bookworm_sway=(wireplumber pipewire-audio-client-libraries)
+# testing_sway(firefox)
 read -p "Do you want to install sway packages from bullseye? [Y/n] " input
 
 if [ "$input" == "y" ] || [ "$input" == "Y" ] || [ "$input" == "" ]; then
@@ -17,7 +19,7 @@ else
   echo "Skipping sway packages..."
 fi
 
-bullseye_coding=(clojure nodejs npm python3-pip)
+bullseye_coding=(clojure leiningen nodejs npm python3-pip postgresql)
 # Missing: babashka
 read -p "Do you want to install coding packages from bullseye? [Y/n] " input
 
@@ -27,4 +29,4 @@ else
   echo "Skipping coding packages..."
 fi
 
-# bookworm_pkgs=(neovim)
+# bookworm_pkgs=(neovim firmware-iwlwifi)
