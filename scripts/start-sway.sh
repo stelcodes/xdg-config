4b273102d1fa98d070aa2cadffe465f0e8918f90
Stel Abrego <stel@stel.codes>
2022-01-18 21:01:13 -0800

    Overhaul sway to be dracula themed, better mappings

--- scripts/start-sway.sh
+++ scripts/start-sway.sh
@@ -1,3 +1,6 @@
 export QT_QPA_PLATFORM=wayland
+export XDG_SESSION_TYPE=wayland
 export XDG_CURRENT_DESKTOP=sway
+# Firefox and tor don't work well under wayland, but firefox needs this to screenshare under sway
+# export MOZ_ENABLE_WAYLAND=1
 exec sway
