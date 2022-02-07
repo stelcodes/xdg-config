090699973e20c2085667fb9db0f2e2b8805615a3
Stel Abrego <stel@stel.codes>
2022-02-07 03:19:51 -0800

    Add fedora packages

--- scripts/fedora-bootstrap.sh
+++ scripts/fedora-bootstrap.sh
@@ -4,7 +4,8 @@ sudo dnf install \
 neovim bash bash-completion fish starship git tree bat fd-find ripgrep wget neofetch curl htop jq tldr unzip rsync restic trash-cli \
 google-noto-sans-mono-fonts google-noto-emoji-color-fonts fontawesome-fonts \
 make g++ clojure rust cargo nodejs \
-firefox kitty keepassxc dolphin torbrowser-launcher \
+firefox kitty keepassxc dolphin \
+torbrowser-launcher protonvpn-cli qbittorrent \
 sway mako xdg-desktop-portal-wlr slurp wl-clipboard wtype libinput-utils grim  wofi \
 i3 setxkbmap rofi slop maim feh \
 kvantum qt5ct gammastep brightnessctl papirus-icon-theme \
