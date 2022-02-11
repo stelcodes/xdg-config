84ccb97f0d70f847fd7825a1645b7240e7a4b2e9
Stel Abrego <stel@stel.codes>
2022-02-10 20:35:08 -0800

    Add packages

--- scripts/fedora-bootstrap.sh
+++ scripts/fedora-bootstrap.sh
@@ -1,12 +1,18 @@
 #! /usr/bin/env bash
 
+sudo systemctl disable cockpit
+sudo systemctl disable cockpit.socket
+
 sudo dnf install \
-neovim bash bash-completion fish starship git tree bat fd-find ripgrep wget neofetch curl htop jq tldr unzip rsync restic trash-cli \
+bash bash-completion fish starship \
+neovim git tree bat fd-find ripgrep wget neofetch curl htop jq tldr unzip rsync restic trash-cli \
 google-noto-sans-mono-fonts google-noto-emoji-color-fonts fontawesome-fonts \
 make g++ clojure rust cargo nodejs \
 firefox kitty keepassxc dolphin \
-torbrowser-launcher protonvpn-cli qbittorrent \
-sway mako xdg-desktop-portal-wlr slurp wl-clipboard wtype libinput-utils grim  wofi \
+torbrowser-launcher qbittorrent \
+openvpn NetworkManager-openvpn NetworkManager-openvpn-gnome gnome-keyring \
+sway mako xdg-desktop-portal-wlr slurp wl-clipboard wtype  grim wofi wlsunset \
 i3 setxkbmap rofi slop maim feh \
-kvantum qt5ct gammastep brightnessctl papirus-icon-theme \
-wireplumber
+kvantum qt5ct brightnessctl papirus-icon-theme libnotify libinput-utils \
+wireplumber bluez blueman pavucontrol \
+mpv
