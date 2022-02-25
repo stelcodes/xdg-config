d1413318312843ad722a3a0008fc444bdf00684b
Stel Abrego <stel@stel.codes>
2022-02-24 19:16:05 -0800

    Add fish aliases

--- fish/config.fish
+++ fish/config.fish
@@ -38,6 +38,7 @@ if status is-interactive
   if command -s fdfind &> /dev/null
     alias fd="fdfind"
   end
-  alias r "rsync -avv --progress"
+  alias r "rsync --archive --verbose --progress --ignore-existing"
   alias s "source ~/.config/fish/config.fish && echo 'config reloaded ✨'"
+  alias yt "yt-dlp -f bestaudio+bestvideo --merge-output-format 'webm'"
 end
