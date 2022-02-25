be4528973052a903ccbf9ee056bba20e80e018eb
Stel Abrego <stel@stel.codes>
2022-02-24 21:33:43 -0800

    Add rpm fusion init script

---
+++ scripts/rpm-fusion.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env sh
+sudo dnf install \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
