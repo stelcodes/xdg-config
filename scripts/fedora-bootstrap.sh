6b6656ca1b9d5d33eddec11ee6e0cbbd255e9e17
Stel Abrego <stel@stel.codes>
2022-02-24 21:31:55 -0800

    Improve fedora bootstrap script

--- scripts/fedora-bootstrap.sh
+++ scripts/fedora-bootstrap.sh
@@ -8,11 +8,15 @@ bash bash-completion fish starship \
 neovim git tree bat fd-find ripgrep wget neofetch curl htop jq tldr unzip rsync restic trash-cli \
 google-noto-sans-mono-fonts google-noto-emoji-color-fonts fontawesome-fonts fontawesome5-fonts \
 make g++ \
-firefox kitty keepassxc dolphin \
+firefox kitty keepassxc dolphin gnome-font-viewer \
 torbrowser-launcher qbittorrent \
 openvpn NetworkManager-openvpn NetworkManager-openvpn-gnome gnome-keyring \
 sway mako xdg-desktop-portal-wlr slurp wl-clipboard wtype  grim wofi wlsunset \
 i3 setxkbmap rofi slop maim feh \
 kvantum qt5ct brightnessctl papirus-icon-theme libnotify libinput-utils \
-wireplumber bluez blueman pavucontrol \
-mpv
+wireplumber bluez blueman pavucontrol
+
+# mpv not in main package channel
+
+# Rebuild font cache
+fc-cache -f
