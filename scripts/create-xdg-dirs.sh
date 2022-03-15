b08295cc5d54e46762c022f84a73544e828e3ada
Stel Abrego <stel@stel.codes>
2022-03-15 15:28:02 -0700

    Improve lowercase-xdg-user-dirs script

--- scripts/create-xdg-dirs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-mkdir $HOME/desktop
-mkdir $HOME/documents
-mkdir $HOME/downloads
-mkdir $HOME/music
-mkdir $HOME/pictures
-mkdir $HOME/public
-mkdir $HOME/template
-mkdir $HOME/videos
---
+++ scripts/lowercase-xdg-user-dirs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+mv --no-clobber `xdg-user-dir DESKTOP` $HOME/desktop
+mv --no-clobber `xdg-user-dir DOCUMENTS` $HOME/documents
+mv --no-clobber `xdg-user-dir DOWNLOADS` $HOME/downloads
+mv --no-clobber `xdg-user-dir MUSIC` $HOME/music
+mv --no-clobber `xdg-user-dir PICTURES` $HOME/pictures
+mv --no-clobber `xdg-user-dir PUBLIC` $HOME/public
+mv --no-clobber `xdg-user-dir TEMPLATE` $HOME/template
+mv --no-clobber `xdg-user-dir VIDEOS` $HOME/videos
