13212004879dc653c334a511ca2622426b516190
Stel Abrego <stel@stel.codes>
2022-03-20 16:23:35 -0700

    Add fisher and fzf.fish plugin

---
+++ scripts/install-fish-plugins.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+# Both the install and update commands download new versions of all the files so this script
+# can be run over and over again without negative side effects
+
+fisher install jorgebucaran/fisher
+fisher install PatrickF1/fzf.fish
+
---
+++ scripts/install-fisher.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
