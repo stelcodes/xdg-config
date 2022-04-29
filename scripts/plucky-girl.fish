03fb9024570169b801d1a50a65b053371b1d0d04
Stel Abrego <stel@stel.codes>
2022-04-28 19:39:35 -0700

    Remove swaynag command

--- scripts/plucky-girl.fish
+++ scripts/plucky-girl.fish
@@ -2,6 +2,5 @@
 if curl --silent https://www.pluckygirl.com | grep -q 'not taking New clients'
   echo '{"text": ""}'
 else
-  swaynag --message 'Call Plucky Girl!!!'
   echo '{"text": "PG New Patient Opening!!!", "state": "Critical"}'
 end
