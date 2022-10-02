867ca3e558673145a1af2ce01ee5790fc3ef37f0
Stel Abrego <stel@stel.codes>
2022-10-02 12:07:45 -0700

    scripts: remove plucky-girl script

--- scripts/plucky-girl.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-if curl --silent https://www.pluckygirl.com | grep -q 'not taking New clients'
-  echo '{"text": ""}'
-else
-  echo '{"text": "PG New Patient Opening!!!", "state": "Critical"}'
-end
