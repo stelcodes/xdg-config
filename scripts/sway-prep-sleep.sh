01214515d359e53dd333824af834dc4bade2c580
Stel Abrego <stel@stel.codes>
2022-10-17 13:05:21 -0700

    sway: fix swaylock screen flicker issue
    When disabling and output and then locking the screen, swaylock will
    flicker the session screen upon resume. This means anyone could look at
    your screen contents even though it was locked. Using dpms on/off
    instead fixes this issue.

--- scripts/sway-prep-sleep.sh
+++ scripts/sway-prep-sleep.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 playerctl --all-players pause
-swaymsg output "*" disable
+# Use dpms off/on when sleeping because just using enable/disable flickers session screen when locked
+# https://github.com/swaywm/swaylock/issues/185
+swaymsg output "*" dpms off
 source ~/.config/scripts/sway-lock-screen.sh
