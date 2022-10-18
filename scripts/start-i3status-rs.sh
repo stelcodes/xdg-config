c5e56f59619a9521b6945a78e625c23e2fd65723
Stel Abrego <stel@stel.codes>
2022-10-17 21:02:39 -0700

    scripts: fix start-i3status-rs.sh weather api key bug

--- scripts/start-i3status-rs.sh
+++ scripts/start-i3status-rs.sh
@@ -3,7 +3,7 @@
 OWM_KEY_FILE="$HOME/sync/secrets/openweathermap-api-key"
 
 if [[ -f $OWM_KEY_FILE ]]; then
-  export OPENWEATHERMAP_API_KEY $(cat $OWM_KEY_FILE)
+  export OPENWEATHERMAP_API_KEY=$(cat $OWM_KEY_FILE)
 fi
 
 i3status-rs 2>> /tmp/i3status-rs-errors.log
