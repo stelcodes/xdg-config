01e16c1ad2de5c70e56c3a0bcb920523bf9c15c5
Stel Abrego <stel@stel.codes>
2022-10-25 23:16:04 -0700

    scripts: fix sway laptop position when scale changes

--- scripts/sway-toggle-laptop-resolution.clj
+++ scripts/sway-toggle-laptop-resolution.clj
@@ -6,8 +6,8 @@
 
 (defmacro debug [sym] `(do (println ~(keyword sym)) (pp/pprint ~sym) (println)))
 
-(defn set-output-scale [output scale]
-  (p/sh ["swaymsg" "output" (:name output) "scale" scale]))
+(defn set-output-scale [output scale pos-x]
+  (p/sh ["swaymsg" "output" (:name output) "scale" scale "pos" (int pos-x) "0"]))
 
 (def outputs
   (-> (p/sh ["swaymsg" "-t" "get_outputs"])
@@ -17,10 +17,11 @@
 (debug outputs)
 
 (def laptop (some #(when (= "eDP-1" (:name %)) %) outputs))
+(def laptop-width (-> laptop :current_mode :width))
 
 (debug laptop)
 
 (if (not= 1.5 (:scale laptop))
-  (set-output-scale laptop 1.5)
-  (set-output-scale laptop 1))
+  (set-output-scale laptop 1.5 (- laptop-width (/ laptop-width 1.5)))
+  (set-output-scale laptop 1 0))
 
