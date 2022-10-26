cf5066381eaf41e46d249197ddf83533e3ebb7f8
Stel Abrego <stel@stel.codes>
2022-10-25 23:24:05 -0700

    sway: make swaylock systemd user service

--- scripts/sway-prep-sleep.sh
+++ scripts/sway-prep-sleep.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 playerctl --all-players pause
+
 # Use dpms off/on when sleeping because just using enable/disable flickers session screen when locked
 # https://github.com/swaywm/swaylock/issues/185
 swaymsg output "*" dpms off
-source ~/.config/scripts/sway-lock-screen.sh
+
+systemctl --user start swaylock
