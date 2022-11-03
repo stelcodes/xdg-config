4891fa731fb4a4e5109f0c3c165833eb91f8dd9b
Stel Abrego <stel@stel.codes>
2022-11-02 18:58:45 -0700

    fish: add nnn alias and environment settings

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,10 @@ if status is-interactive
   set -x BAT_THEME 'base16'
   # fzf dracula color scheme
   set -x FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+  # nnn settings
+  set -x NNN_TRASH 1
+  set -x NNN_PLUG 'p:preview-tui'
+  alias n "nnn -eauUHP p"
   # Aliases
   alias ll 'ls -l'
   alias la 'ls --almost-all'
