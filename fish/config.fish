41014a4db0390c390037d2d99660d4dad40afccf
Stel Abrego <stel@stel.codes>
2022-11-03 18:24:22 -0700

    nnn: customize dragdrop plugin

--- fish/config.fish
+++ fish/config.fish
@@ -18,7 +18,7 @@ if status is-interactive
   set -x FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
   # nnn settings
   set -x NNN_TRASH 1
-  set -x NNN_PLUG 'p:preview-tui'
+  set -x NNN_PLUG 'p:preview-tui;d:dragdrop'
   set -x NNN_TMPFILE '/tmp/nnn-last-dir'
   function n
     nnn -eauUHP p
