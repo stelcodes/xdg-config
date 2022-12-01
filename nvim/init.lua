a3399368dd5a4f46959fef5ba2aae1b98296d8d9
Stel Abrego <stel@stel.codes>
2022-11-30 20:10:55 -0800

    nvim: add nvim-cmp and luasnips

--- nvim/init.lua
+++ nvim/init.lua
@@ -490,7 +490,186 @@ packer.startup(function(use)
     end
   }
 
-  -- TODO https://github.com/hrsh7th/nvim-cmp
+  use {
+    'L3MON4D3/LuaSnip',
+    requires = {
+      'rafamadriz/friendly-snippets'
+    },
+    config = function()
+      local ls = require "luasnip"
+      local types = require "luasnip.util.types"
+
+      ls.config.set_config {
+        -- This tells LuaSnip to remember to keep around the last snippet.
+        -- You can jump back into it even if you move outside of the selection
+        history = true,
+
+        -- This one is cool cause if you have dynamic snippets, it updates as you type!
+        updateevents = "TextChanged,TextChangedI",
+
+        -- Autosnippets:
+        enable_autosnippets = true,
+
+        -- Crazy highlights!!
+        -- #vid3
+        -- ext_opts = nil,
+        ext_opts = {
+          [types.choiceNode] = {
+            active = {
+              virt_text = { { " « ", "NonTest" } },
+            },
+          },
+        },
+      }
+      vim.keymap.set({ "i", "s" }, "<c-k>", function()
+        if ls.expand_or_jumpable() then
+          ls.expand_or_jump()
+        end
+      end, { silent = true })
+
+      -- <c-j> is my jump backwards key.
+      -- this always moves to the previous item within the snippet
+      vim.keymap.set({ "i", "s" }, "<c-j>", function()
+        if ls.jumpable(-1) then
+          ls.jump(-1)
+        end
+      end, { silent = true })
+
+      -- Load vs_code snippets from plugins
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end
+  }
+
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'onsails/lspkind.nvim'
+    },
+    config = function()
+      local lspkind = require('lspkind')
+      lspkind.init()
+      local cmp = require('cmp')
+      cmp.setup {
+        mapping = {
+          ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<c-y>"] = cmp.mapping(
+            cmp.mapping.confirm {
+              behavior = cmp.ConfirmBehavior.Insert,
+              select = true,
+            },
+            { "i", "c" }
+          ),
+          ["<c-space>"] = cmp.mapping {
+            i = cmp.mapping.complete(),
+            c = function(
+              _ --[[fallback]]
+            )
+              if cmp.visible() then
+                if not cmp.confirm { select = true } then
+                  return
+                end
+              else
+                cmp.complete()
+              end
+            end,
+          },
+          ["<tab>"] = cmp.config.disable,
+          -- Testing
+          ["<c-q>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+        },
+
+        sources = {
+          { name = "gh_issues" },
+
+          -- Youtube: Could enable this only for lua, but nvim_lua handles that already.
+          { name = "nvim_lua" },
+
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "luasnip" },
+          { name = "buffer", keyword_length = 5 },
+        },
+
+        sorting = {
+          -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
+          comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+
+            -- copied from cmp-under, but I don't think I need the plugin for this.
+            -- I might add some more of my own.
+            function(entry1, entry2)
+              local _, entry1_under = entry1.completion_item.label:find "^_+"
+              local _, entry2_under = entry2.completion_item.label:find "^_+"
+              entry1_under = entry1_under or 0
+              entry2_under = entry2_under or 0
+              if entry1_under > entry2_under then
+                return false
+              elseif entry1_under < entry2_under then
+                return true
+              end
+            end,
+
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
+
+        -- Youtube: mention that you need a separate snippets plugin
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+
+        formatting = {
+          -- Youtube: How to set up nice formatting for your sources.
+          format = lspkind.cmp_format {
+            with_text = true,
+            menu = {
+              buffer = "[buf]",
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[api]",
+              path = "[path]",
+              luasnip = "[snip]",
+              gh_issues = "[issues]",
+              tn = "[TabNine]",
+            },
+          },
+        },
+
+        experimental = {
+          -- I like the new menu better! Nice work hrsh7th
+          native_menu = false,
+
+          -- Let's play with this for a day or two
+          ghost_text = false,
+        },
+
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+      }
+
+    end
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Must be ran after all plugins are defined
