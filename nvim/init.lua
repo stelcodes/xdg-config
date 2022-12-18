209dacd1023bec7b539969c433131ea3a1ccbebb
Stel Abrego <stel@stel.codes>
2022-12-17 19:29:25 -0800

    nvim: add listchars for visible whitespace

--- nvim/init.lua
+++ nvim/init.lua
@@ -432,7 +432,14 @@ packer.startup(function(use)
   use {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
-      require("indent_blankline").setup {}
+      vim.opt.list = true
+      vim.opt.listchars:append "space:⋅"
+      vim.opt.listchars:append "eol:↴"
+      vim.opt.listchars:append "tab:»»"
+      -- require("indent_blankline").setup {
+      --   show_end_of_line = true,
+      --   space_char_blankline = " ",
+      -- }
     end
   }
 
