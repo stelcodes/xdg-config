a84d7219af5a3e8e62a8287d5d579b24971470fa
Stel Abrego <stel@stel.codes>
2023-02-17 15:46:41 -0800

    scripts: update swaylock script with hydropump theme

--- scripts/sway-lock-screen.sh
+++ scripts/sway-lock-screen.sh
@@ -4,10 +4,10 @@
 # https://github.com/swaywm/swaylock/issues/223
 
 swaylock \
-  --image ~/wallpaper \
+  --image ~/wallpaper.png \
   --font 'JetBrains Mono' --font-size 14 \
-  --line-color 21222c --text-color 21222c \
-  --inside-color 6272a4 --ring-color 6272a4 \
-  --inside-clear-color ffffa5 --inside-ver-color bd93f9 --inside-wrong-color ff6e6e \
-  --bs-hl-color ff5555 --key-hl-color 50fa7b \
-  --ring-ver-color bd93f9 --ring-wrong-color ff5555 --ring-clear-color f1fa8c
+  --line-color 1d212a --text-color 1d212a \
+  --inside-color b2caff --ring-color b2caff \
+  --inside-clear-color dafd89 --inside-ver-color 4bead2 --inside-wrong-color f05c8e \
+  --bs-hl-color ff3c7e --key-hl-color 9bb9fc \
+  --ring-ver-color 18eece --ring-wrong-color ff3c7e --ring-clear-color dafd89
